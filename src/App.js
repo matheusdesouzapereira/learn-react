fe293f415c58b3214377a0dd2c0199477c966e71
matheus-pereira <matheus@on2.dev>
2023-05-08 10:39:36 -0300

    colocando em pratica aprendizado de React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <p>React JS</p>
       </header>
+
+      <main>
+        <section className="box-titulo">
+          <h1>React JS</h1>
+          <p>Biblioteca JavaScript para criar SPAs</p>
+
+          <a href="https://reactjs.org/" target="blank">
+            Site Oficial
+          </a>
+        </section>
+
+        <section className="box-requisitos">
+          <h2>Requisitos</h2>
+          <p>Veja abaixo os requisitos mínimos para apredner React JS:</p>
+
+          <div className="box-cards">
+            <div className="card">
+              <h3>HTML</h3>
+              <p>Apredner a estruturar os elementos da página com HTML</p>
+            </div>
+
+            <div className="card">
+              <h3>CSS</h3>
+              <p>Aprender a estilizar os elementos da página com CSS</p>
+            </div>
+
+            <div className="card">
+              <h3>JavaScript</h3>
+              <p>Aprender a linguagem utilizada pelo React JS, o JavaScript</p>
+            </div>
+          </div>
+        </section>
+      </main>
+      
+      <footer>
+        <p className="autor">Curso de instrodução de React JS | By Matheus Pereira</p>
+      </footer>
     </div>
   );
 }
